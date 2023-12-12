a9ddd3d3a613e1f7cacf415149060e0de011a53f
Bowarc <yuyeaaaahhhh@laposte.net>
2023-12-12 22:56:42 +0100

    Added css, fixed build.sh to replace html & css content on write. Added lock file

--- build.sh
+++ build.sh
@@ -17,24 +17,55 @@ fi
 
 cp ./target/wasm-bindgen/debug/* ./out 
 
-cat << EOF >> ./out/index.html
-<!DOCTYPE html><html lang='en'>
-<head>
-  <meta charset='utf-8'>
-  <title>Yew • Counter</title>
-
-  <script type='module'>
-    import init from './yew_front.js';
-    init('yew_front_bg.wasm');
-  </script>
-  
-  <!--   <link rel='preload' href='./yew_front_bg.wasm' as='fetch' type='application/wasm' crossorigin=''>
-    <link rel='modulepreload' href='./yew_front.js'> -->
-</head>
+cat << EOF > ./out/index.html
+<!DOCTYPE html>
+<html lang='en'>
+  <head>
+    <meta charset='utf-8'>
+    <title>Yew • Counter</title>
+    <link rel="stylesheet" type="text/css" href="./style.css">
+    <script type='module'>
+      import init from './yew_front.js';
+      init('yew_front_bg.wasm');
+    </script>
+    
+    <!--   <link rel='preload' href='./yew_front_bg.wasm' as='fetch' type='application/wasm' crossorigin=''>
+      <link rel='modulepreload' href='./yew_front.js'> -->
+  </head>
   <body>
   </body>
 </html>
 EOF
+
+cat << EOF > ./out/style.css
+button {
+  background-color: #008f53; /* Green */
+  border: 0;
+  color: white;
+  padding: 14px 14px;
+  text-align: center;
+  font-size: 16px;
+  margin: 2px 2px;
+  width: 100px;
+}
+
+.counter {
+  color: #008f53;
+  font-size: 48px;
+  text-align: center;
+}
+
+.footer {
+  text-align: center;
+  font-size: 12px;
+}
+
+.panel {
+  display: flex;
+  justify-content: center;
+}
+EOF
+
 echo Everything should be ready in ./out
 
 echo Python demo server at http://localhost:8000/out
